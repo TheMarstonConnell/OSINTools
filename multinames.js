3e5c46beed88e3ee389811a461d05f241fa75dfd
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2021-07-27 16:39:04 -0400

    Scalability fixes

--- multinames.js
+++ multinames.js
@@ -1,26 +1,3 @@
-let services = {
-    spotify: 'https://open.spotify.com/user/{NAME}',
-    wattpad: 'https://www.wattpad.com/user/{NAME}',
-    disqus: 'https://disqus.com/by/{NAME}',
-    lastfm: 'https://last.fm/user/{NAME}',
-    livejournal: 'https://{NAME}.livejournal.com/',
-    poshmark: 'https://poshmark.com/closet/{NAME}',
-    myspace: 'https://myspace.com/{NAME}',
-    dailymotion: 'https://www.dailymotion.com/{NAME}',
-    steam: 'https://steamcommunity.com/id/{NAME}',
-    twitter: 'https://twitter.com/{NAME}',
-    vsco: 'https://vsco.co/{NAME}/gallery',
-    pinterest: 'https://www.pinterest.ca/{NAME}',
-    instagram: 'https://instagram.com/{NAME}/',
-    ebay: 'https://www.ebay.com/usr/{NAME}',
-    reddit: 'https://reddit.com/user/{NAME}',
-    gravatar: 'http://gravatar.com/{NAME}',
-    smule: 'https://smule.com/{NAME}',
-    tumblr: 'https://{NAME}.tumblr.com',
-    wordpress: 'https://{NAME}.wordpress.com',
-
-}
-
 function list_services(){
     let sortable = [];
     for (let key in services) {
